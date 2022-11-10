33d5160ac8f234dba57030c3bacf8bd878c0e708
Alexandre Ruban <alexandre@hey.com>
2022-11-10 18:23:45 +0100

    [GENERATED]: Install ActionMarkdown in test/dummy
    This commit was generated by the following commands:
    
    ```rb
    cd test/dummy
    rails action_markdown:install
    rails db:migrate
    ```

---
+++ test/dummy/db/schema.rb
@@ -0,0 +1,24 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_172331) do
+  create_table "action_markdown_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_markdown_rich_texts_uniqueness", unique: true
+  end
+
+end
